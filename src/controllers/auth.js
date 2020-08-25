96225841adbb22e067c73d19af2c0bd201e1813f
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-08-25 12:35:33 -0300

    Storing accounts into db

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const { Account } = require('../models')
 const router = express.Router();
+const saltRounds = 10
 
 // Alterar Futuramente para POST | Rotas de login e cadastro sao realizadas em post
 router.get('/login', (req, res) => {
   return res.json('Login')
 });
 
-router.get('/cadastro', (req, res) => {
-  return res.json('Cadastro')
+router.get('/cadastro', async (req, res) => {
+  const email = 'vncsmntr@outlook.com';
+  const password = 'root'
+  
+  const hash = bcrypt.hashSync(password, saltRounds)
+
+  const result = await Account.create({email, password: hash});
+
+  return res.json(result)
 });
 
 module.exports = router;
