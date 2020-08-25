faa2bdb6f06e4ee74d73d75c33a2cd23b42499f6
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-08-25 11:51:31 -0300

    Connected with MySQL

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const authController = require('./controllers/auth');
+const db = require('./models');
+
 
 const app = express();
 app.use('/auth', authController);
@@ -9,6 +11,8 @@ app.get('/', (req, res) => {
   return res.json('API Online')
 });
 
-app.listen(3001, () => {
-  console.log('Server Online')
+db.sequelize.sync().then( ()=>{
+  app.listen(3001, () => {
+    console.log('Server Online')
+  });
 });
