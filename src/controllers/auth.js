8d7ad7945c45ea7798d74491e7d69d928ced9dd7
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-09-01 15:20:19 -0300

    Add JWT to API

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,24 +2,39 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const { Account } = require("../models");
-const { accountSignUp } = require("../validators/account");
+const { accountSignUp, accountSignIn } = require("../validators/account");
 const { getMessage } = require("../helpers/messages");
+const { generateJwt, generateRefreshJwt } = require('../helpers/jwt');
+
 
 const saltRounds = 10;
 // Alterar Futuramente para POST | Rotas de login e cadastro sao realizadas em post
-router.get("/login", (req, res) => {
-  return res.jsonOK();
+router.post("/sign-in", accountSignIn, async(req, res) => {
+    const { email, password } = req.body;
+    const account = await Account.findOne({ where: { email } });
+    if (!account) return res.jsonBadRequest(null, getMessage('account.signin.invalid'));
+
+    //! Validar Senha
+    const match = account ? bcrypt.compareSync(password, account.password) : null;
+    if (!match) return res.jsonBadRequest(null, getMessage('account.signin.invalid'));
+
+    const token = generateJwt({ id: account.id });
+    const refreshToken = generateRefreshJwt({ id: account.id });
+
+    return res.jsonOK(account, getMessage("account.signin.success"), { token, refreshToken });
 });
 
-router.get("/sign-up", accountSignUp, async (req, res) => {
-  const { email, password } = req.body;
-  const account = await Account.findOne({ where: { email } });
-  if (account)
-    return res.jsonBadRequest(null, getMessage("account.signup.email_exists"));
-  const hash = bcrypt.hashSync(password, saltRounds);
-  const newAccount = await Account.create({ email, password: hash });
+router.post("/sign-up", accountSignUp, async(req, res) => {
+    const { email, password } = req.body;
+    const account = await Account.findOne({ where: { email } });
+    if (account)
+        return res.jsonBadRequest(null, getMessage("account.signup.email_exists"));
+    const hash = bcrypt.hashSync(password, saltRounds);
+    const newAccount = await Account.create({ email, password: hash });
+    const token = generateJwt({ id: newAccount.id });
+    const refreshToken = generateRefreshJwt({ id: newAccount.id });
 
-  return res.jsonOK(newAccount, getMessage("account.signup.success"));
+    return res.jsonOK(newAccount, getMessage("account.signup.success"), { token, refreshToken });
 });
 
 module.exports = router;
