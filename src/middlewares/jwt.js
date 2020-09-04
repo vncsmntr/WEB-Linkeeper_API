bae2b8be9aeca1f5fc314d81b78ba589f6554ac5
Vinicius Monteiro <vncsmntr@outlook.com>
2020-09-04 16:19:35 -0300

    Excluded auth path from JWT

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -2,6 +2,16 @@ const { verifyJwt } = require('../helpers/jwt');
 const { decode } = require('jsonwebtoken');
 
 const checkJwt = (req, res, next) => {
+
+    const { url: path } = req;
+    const excludedPaths = [
+        '/auth/sign-in',
+        '/auth/sign-up'
+    ]
+
+    const isExcluded = !!excludedPaths.find(p => p.startsWith(path));
+    if (isExcluded) return next();
+
     let token = req.headers['authorization'];
     token = token ? token.slice(7, token.length) : null;
     if (!token) {
